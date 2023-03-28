bc7ae923a6796fad5096a80e8f0e1686ff52af71
minhquan <minhquan.gv.hcm@gmail.com>
2023-03-28 16:16:38 +0700

    css login register page

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,10 +1,149 @@
 import './Authentication.scss';
 
+import { useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom';
+// import toast from 'react-hot-toast';
+
+const toast_success = {
+    style: {
+        padding: '1rem'
+    },
+    iconTheme: {
+        primary: '#087B44'
+    }
+}
+
+const toast_error = {
+    style: {
+        padding: '1rem'
+    },
+    iconTheme: {
+        primary: '#dd2222'
+    }
+}
+
 const Login = () => {
+
+    let history = useHistory();
+
+    const [loginData, setLoginData] = useState('');
+    const [password, setPasssword] = useState('');
+
+    const [validLogin, setValidLogin] = useState(true);
+    const [validPassword, setValidPassword] = useState(true);
+
+    const handleNavigateToRegisterPage = () => {
+        history.push('/register');
+    }
+
+    const resetValidData = () => {
+        setValidLogin(true);
+        setValidPassword(true);
+    }
+
+    const handleLogin = async () => {
+
+        // resetValidData();
+        // if (!loginData) {
+        //     setValidLogin(false);
+        //     toast.error("Email or phone is empty !");
+        // }
+        // if (!password) {
+        //     setValidPassword(false);
+        //     toast.error("Password is empty !");
+        // }
+
+        // let result = await userLogin(loginData, password);
+        // if (result) {
+        //     let res = result;
+        //     if (+res.EC === 0) {
+        //         toast.success(res.EM);
+
+        //         let accessToken = res.DT.accessToken;
+        //         let groupWithRoles = res.DT.groupWithRoles;
+        //         let email = res.DT.email;
+        //         let username =res.DT.username;
+
+        //         let data = {
+        //             isAuthenticated: true,
+        //             token: accessToken,
+        //             account: {email, username, groupWithRoles}
+        //         }
+
+        //         userContextlogin(data);
+        //         history.push('/users');
+
+        //     } else {
+        //         toast.error(res.EM);
+        //     }
+        // }
+
+    }
+
+    const handleKeyPress = (event) => {
+        // if(event.key === 'Enter') {
+        //     handleLogin();
+        // }
+    }
+
+    // if(user && user.isAuthenticated === true) {
+    //     history.push('/users');
+    // }
+
     return (
-        <div>
-            Login
+        <div className='loginPage'>
+            <div className="container d-flex flex-row login-container py-5">
+                <div className="content-left col-7 d-none d-sm-block  px-5">
+                    <div className='brand fs-1'>
+                        Megabook
+                    </div>
+                    <div className='content fs-3'>
+                        Megabook where helps you find books and boosts your knowledge
+                    </div>
+                </div>
+                <div className="content-right d-flex flex-column col-12 col-sm-5 p-3 gap-3">
+                    <div className='brand text-center fs-1 d-sm-none d-block'>
+                        Megabook
+                    </div>
+                    <input
+                        type="text"
+                        className={validLogin === true ? "form-control px-3 py-3 " : "form-control px-3 py-3 is-invalid"}
+                        placeholder='Email address or phone number'
+                        value={loginData}
+                        onChange={(event) => setLoginData(event.target.value)}
+                        onKeyPress={(event)=> handleKeyPress(event)}
+                    />
+                    <input
+                        type="password"
+                        className={validPassword === true ? "form-control px-3 py-3 " : "form-control px-3 py-3 is-invalid"}
+                        placeholder='Password'
+                        value={password}
+                        onChange={(event) => setPasssword(event.target.value)}
+                        onKeyPress={(event)=> handleKeyPress(event)}
+                    />
+                    <button
+                        className='btn btn-primary fs-6 login-btn py-2'
+                        onClick={() => handleLogin()}
+                    >
+                        Log in
+                    </button>
+                    <span className='text-center forgottenPass'>
+                        <a href='#'>Forgotten password?</a>
+                    </span>
+                    <hr />
+                    <div className='text-center pb-3'>
+                        <button
+                            className='btn fs-6 register-btn px-3'
+                            onClick={() => handleNavigateToRegisterPage()}
+                        >
+                            Create New Account
+                        </button>
+                    </div>
+
+                </div>
+            </div>
         </div>
+
     )
 }
 
--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -1,10 +1,188 @@
 import './Authentication.scss';
 
+import { useHistory } from 'react-router-dom';
+import {useState } from 'react';
+// import toast from 'react-hot-toast';
+
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [reEnterPassword, setReEnterPassword] = useState('');
+
+
+    const [validEmail, setValidEmail] = useState(true);
+    const [validUsername, setValidUsername] = useState(true);
+    const [validPhone, setValidPhone] = useState(true);
+    const [validPassword, setValidPassword] = useState(true);
+    const [validReEnterPassword, setValidReEnterPassword] = useState(true);
+
+    let history = useHistory();
+
+    const handleNavigateToLoginPage = () => {
+        history.push('/login');
+    }
+
+    const resetValidInput = () => {
+        setValidEmail(true);
+        setValidUsername(true);
+        setValidPhone(true);
+        setValidPassword(true);
+        setValidReEnterPassword(true);
+    }
+
+    const handleRegister = async () => {
+
+        // resetValidInput();
+
+        // if (!email) {
+        //     setValidEmail(false);
+        //     toast.error("email is empty !");
+        //     return;
+        // }
+
+        // if (!username) {
+        //     setValidUsername(false);
+        //     toast.error("username is empty !");
+        //     return;
+        // }
+
+        // if (!phone) {
+        //     setValidPhone(false);
+        //     toast.error("phone is empty !");
+        //     return;
+        // }
+
+        // if (!password) {
+        //     setValidPassword(false);
+        //     toast.error("password is empty !");
+        //     return;
+        // }
+
+        // if (!reEnterPassword) {
+        //     setValidReEnterPassword(false);
+        //     toast.error("confirm password is empty !");
+        //     return;
+        // }
+
+        // if (password !== reEnterPassword) {
+        //     setValidReEnterPassword(false);
+        //     toast.error("confirm password is not the same !");
+        //     return;
+        // }
+
+        // var re = /\S+@\S+\.\S+/;
+        // if (!re.test(email)) {
+        //     setValidEmail(false);
+        //     toast.error("email is invalid !");
+        //     return;
+        // }
+
+        // let result = await userRegister(email, username, password, phone);
+        // let res = result;
+        // if(res) {
+        //     if (+res.EC === 0) {
+        //         toast.success(res.EM);
+        //         history.push('/login');
+        //     } else {
+        //         toast.error(res.EM);
+        //     }
+        // }  
+    }
+
     return (
-        <div>
-            Register
+        <div className='registerPage'>
+            <div className="container d-flex flex-row register-container py-5">
+                <div className="content-left col-7 d-none d-sm-block  px-5">
+                    <div className='brand fs-1'>
+                        Megabook
+                    </div>
+                    <div className='content fs-3'>
+                        Megabook where helps you find books and boosts your knowledge
+                    </div>
+                </div>
+                <div className="content-right d-flex flex-column col-12 col-sm-5 p-3 gap-3">
+                    <div className='brand text-center fs-1 d-sm-none d-block'>
+                        Megabook
+                    </div>
+                    <div>
+                        <label className="form-label">Email:</label>
+                        <input
+                            type="text"
+
+                            className={validEmail === true ? "form-control p-2 " : "form-control p-2 is-invalid"}
+                            placeholder='Email address'
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="form-label">Username:</label>
+                        <input
+                            type="text"
+                            className={validUsername === true ? "form-control p-2 " : "form-control p-2 is-invalid"}
+                            placeholder='Username'
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="form-label">Phone number:</label>
+                        <input
+                            type="text"
+                            className={validPhone === true ? "form-control p-2 " : "form-control p-2 is-invalid"}
+                            placeholder='Phone number'
+                            value={phone}
+                            onChange={(event) => setPhone(event.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="form-label">Password</label>
+                        <input
+                            type="password"
+                            className={validPassword === true ? "form-control p-2 " : "form-control p-2 is-invalid"}
+                            placeholder='Password'
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="form-label">Re-enter password</label>
+                        <input
+                            type="password"
+                            className={validReEnterPassword === true ? "form-control p-2 " : "form-control p-2 is-invalid"}
+                            placeholder='Re-enter password'
+                            value={reEnterPassword}
+                            onChange={(event) => setReEnterPassword(event.target.value)}
+                        />
+                    </div>
+
+                    <button
+                        className='btn btn-primary fs-6 register-btn py-2'
+                        onClick={() => handleRegister()}
+                    >
+                        Register
+                    </button>
+                    <hr />
+                    <div className='text-center pb-3'>
+                        <button
+                            className='btn fs-6 login-btn px-3'
+                            onClick={() => handleNavigateToLoginPage()}
+                        >
+                            Already have account, Log in
+                        </button>
+                    </div>
+
+                </div>
+            </div>
         </div>
+
     )
 }
 
